0bbcfd371e7c56a750b29678c24e0d4f1d0b0e87
Andrey <univer1857@gmail.com>
2021-03-09 15:43:53 +0200

    feat: task solution

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -8,6 +8,8 @@ module.exports = class DepthCalculator {
 
   calculateDepth(arr) {
     return Array.isArray(arr) ?
-      1 + Math.max(...arr.map(this.calculateDepth)) : 0; 
+      1 + arr.reduce((max, item) => {
+        return Math.max(max, this.calculateDepth(item));
+      }, 0) : 0; 
   }
 };
